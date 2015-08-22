578cd5f1b4022185b490de1360d30a3f6f0e8066
Tomek Święcicki <tomislater@gmail.com>
2015-08-22 14:23:18 +0200

    Add constants

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,30 @@ extern crate argparse;
 
 use argparse::{ArgumentParser,Store};
 
+const TODAY: &'static str = "curr";
+const TOMORROW: &'static str = "curr+1";
+const RASP_URL: &'static str = "http://rasp.linta.de/GERMANY/";
+const SUFFIX: &'static str = "lst.d2.png";
+
 
 fn main() {
-	let mut parameter = "".to_string();
-	let mut when = "today".to_string();
+    let mut parameter = "".to_string();
+    let mut when = "today".to_string();
 
-	{
-		let mut ap = ArgumentParser::new();
+	// `parameter` and `when` variables are mutable thus we cannot borrow them more than once at a time
+    {
+        let mut ap = ArgumentParser::new();
 
-		ap.refer(&mut parameter)
-			.add_option(&["--parameter"], Store, "Parameter, please look at http://rasp.linta.de/GERMANY/index_en.html.")
-			.required();
+        ap.refer(&mut parameter)
+            .add_option(&["--parameter"], Store, "Parameter, please look at http://rasp.linta.de/GERMANY/index_en.html.")
+            .required();
 
-		ap.refer(&mut when)
-			.add_option(&["--when"], Store, "When, today or tomorrow.");
+        ap.refer(&mut when)
+            .add_option(&["--when"], Store, "When, today or tomorrow.");
 
-		ap.parse_args_or_exit();
+        ap.parse_args_or_exit();
 	}
 
     println!("Rrasp!");
-	println!("Parameter: {}, When: {}", parameter, when);
+    println!("Parameter: {}, When: {}", parameter, when);
 }

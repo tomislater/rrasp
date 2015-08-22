1393f965e470d273ceba89b5a5df765ab9971c06
Tomek Święcicki <tomislater@gmail.com>
2015-08-22 13:57:42 +0200

    Add argparse package + add argument parser logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser,Store};
+
+
 fn main() {
+	let mut parameter = "".to_string();
+	let mut when = "today".to_string();
+
+	{
+		let mut ap = ArgumentParser::new();
+
+		ap.refer(&mut parameter)
+			.add_option(&["--parameter"], Store, "Parameter, please look at http://rasp.linta.de/GERMANY/index_en.html.")
+			.required();
+
+		ap.refer(&mut when)
+			.add_option(&["--when"], Store, "When, today or tomorrow.");
+
+		ap.parse_args_or_exit();
+	}
+
     println!("Rrasp!");
+	println!("Parameter: {}, When: {}", parameter, when);
 }
